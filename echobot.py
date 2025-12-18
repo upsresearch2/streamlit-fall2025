05164de06d056e25660e597c52c38781b6b7761d
upsresearch2 <41304354+upsresearch2@users.noreply.github.com>
2025-12-18 17:39:11 -0500

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,22 +1,6 @@
 import streamlit as st
-import random
-import time
+import numpy as np
 
-st.title("Simple chat")
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
