928a12a99f78fed1ec562f508e74f345f7413cf0
upsresearch2 <41304354+upsresearch2@users.noreply.github.com>
2025-12-18 18:21:41 -0500

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -55,7 +55,7 @@ st.title("Simple chat")
 def response_generator():
     response = ai_ask("Pretend you are a very friendly and helpful person.  Please provide a response given the provided context.  Please provide the response only with no before or after commentary.  If there is no context, then please respond with a helpful and warm welcome message.",
                       data=st.session_state.messages,
-                      api_key=st.secrets[TrtsvPBHenqX16uLYun7zfxzy5M75YIa])
+                      api_key=st.secrets["apikey"])
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
